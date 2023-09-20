d03a61d6627ca5717f44ed202eb59b949cb0a2a8
parksangbin <zaza1599@gmail.com>
2023-09-20 23:59:49 +0900

    커밋

--- board/src/main/java/com/memberboard/board/board/model/dao/BoardDAO.java
+++ board/src/main/java/com/memberboard/board/board/model/dao/BoardDAO.java
@@ -1,22 +1,22 @@
-package com.memberboard.board.board.model.dao;
-
-import java.util.List;
-
-import org.springframework.ui.Model;
-
-import com.memberboard.board.board.model.vo.Board;
-import com.memberboard.board.board.model.vo.Pagination;
-
-public class BoardDAO {
-
-	public int getListCount(Model model) {
-		
-		return 0;
-	}
-
-	public List<Board> selectBoardList(Pagination pagination, Model model) {
-		
-		return null;
-	}
-
-}
+//package com.memberboard.board.board.model.dao;
+//
+//import java.util.List;
+//
+//import org.springframework.ui.Model;
+//
+//import com.memberboard.board.board.model.vo.Board;
+//import com.memberboard.board.board.model.vo.Pagination;
+//
+//public class BoardDAO {
+//
+//	public int getListCount(Model model) {
+//		
+//		return 0;
+//	}
+//
+//	public List<Board> selectBoardList(Pagination pagination, Model model) {
+//		
+//		return null;
+//	}
+//
+//}
--- board/src/main/webapp/WEB-INF/views/board/boardWrite.jsp
+++ board/src/main/webapp/WEB-INF/views/board/boardWrite.jsp
@@ -7,6 +7,24 @@
 <title>boardWrite</title>
 </head>
 <body>
+<h1>게시글 쓰는 페이지</h1>
+<form action="enroll" method="post">
+    <div class="input_wrap">
+        제목
+        <input type="text" name="title">
+    </div>
+
+    <div class="input_wrap">
+        내용
+        <input type="text" name="content">
+    </div>
+
+    <div class="input_wrap">
+        글쓴이
+        <input type="text" name="writer">
+    </div>
+    <button>등록</button>
+</form>
 
 </body>
 </html>
